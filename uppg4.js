b9673371cca86e78af3b2b97df0f05df9e7f9720
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-05-10 16:02:18 +0200

    Uppgift 4: Done

--- uppg4.js
+++ uppg4.js
@@ -6,7 +6,14 @@ function uppg4() {
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
 
+    const person = {
+        name: "Emil Ejderklev",
+        age: 25
+    };
+    console.log(person.age);
 
 }
+
+uppg4();
 
 module.exports = { uppg4 };
