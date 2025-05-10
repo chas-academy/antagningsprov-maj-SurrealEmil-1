e0cd5e4f317d9c1dc35d951132431d850a167ecb
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-05-10 16:16:15 +0200

    Uppgift 8: Done

--- uppg7.js
+++ uppg7.js
@@ -12,8 +12,7 @@ function uppg7() {
 
     const result = sum(5, 10);
     console.log(result);
-    
-    return result;
+
 }
 
 uppg7();
--- uppg8.js
+++ uppg8.js
@@ -12,6 +12,26 @@ function uppg8(){
 // anropa funktionen och skicka med arrayen som argument
 
 
+    const persons = [
+        { name: "Emil", age: 25 },
+        { name: "John", age: 35 },
+        { name: "Doe", age: 40 },
+        { name: "Jane", age: 28 },
+        { name: "Smith", age: 32 }
+    ];
+
+    function printNamesOver30(arr) {
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i].age > 30) {
+                console.log(arr[i].name);
+            }
+        }
+    }
+
+    printNamesOver30(persons);
+
 }
+
+uppg8();
 
 module.exports = { uppg8 };
