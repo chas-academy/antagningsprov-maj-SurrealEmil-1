0fc0334d736733d23321a10c5b547aee7aeb6a20
SurrealEmil <emil.ejderklev1@hotmail.com>
2025-05-10 16:08:53 +0200

    Uppgift 6: Done

--- uppg6.js
+++ uppg6.js
@@ -5,7 +5,14 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 
+    const names = ["Emil", "Ejderklev", "John", "Doe"];
+
+    for (let i = 0; i < names.length; i++) {
+        console.log(names[i]);
+    }
  
 }
+
+uppg6();
 
 module.exports = { uppg6 };
